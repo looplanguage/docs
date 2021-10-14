fc1b4bddc9f8055afed79e01fcc63e4da6c2d1ae
kanersps <12296463+kanersps@users.noreply.github.com>
2021-10-14 15:51:35 +0200

    feat: integrate algolia for the future

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,108 +4,123 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'Loop Language',
-  tagline: 'Modern dynamic type-safe, programming language.',
-  url: 'https://looplang.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    title: 'Loop Language',
+    tagline: 'Modern dynamic type-safe, programming language.',
+    url: 'https://looplang.org',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'facebook', // Usually your GitHub org/user name.
+    projectName: 'docusaurus', // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit/blog',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+      [
+        '@docusaurus/preset-classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            // Please change this to your repo.
+            editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit',
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit/blog',
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        }),
+      ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Loop Language',
-        logo: {
-          alt: 'Loop Logo',
-          src: 'img/logo.svg',
-        },
-        items: [{
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {
-            to: '/blog',
-            label: 'Updates',
-            position: 'left'
-          },
-          {
-            href: 'https://github.com/looplanguage',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [{
-            title: 'Docs',
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+          navbar: {
+            title: 'Loop Language',
+            logo: {
+              alt: 'Loop Logo',
+              src: 'img/logo.svg',
+            },
             items: [{
-              label: 'Introduction',
-              to: '/docs/introduction',
-            }, ],
-          },
-          {
-            title: 'Community',
-            items: [{
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                type: 'doc',
+                docId: 'intro',
+                position: 'left',
+                label: 'Docs',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [{
-                label: 'Blog',
                 to: '/blog',
+                label: 'Updates',
+                position: 'left'
               },
               {
+                href: 'https://github.com/looplanguage',
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                position: 'right',
               },
             ],
           },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Loop Language`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+          themeConfig: {
+            // ...
+            algolia: {
+              apiKey: 'YOUR_API_KEY',
+              indexName: 'YOUR_INDEX_NAME',
+
+              // Optional: see doc section below
+              contextualSearch: true,
+
+              // Optional: see doc section below
+              appId: 'YOUR_APP_ID',
+
+              // Optional: Algolia search parameters
+              searchParameters: {},
+            },
+            footer: {
+              style: 'dark',
+              links: [{
+                  title: 'Docs',
+                  items: [{
+                    label: 'Introduction',
+                    to: '/docs/introduction',
+                  }, ],
+                },
+                {
+                  title: 'Community',
+                  items: [{
+                      label: 'Stack Overflow',
+                      href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                    },
+                    {
+                      label: 'Discord',
+                      href: 'https://discordapp.com/invite/docusaurus',
+                    },
+                    {
+                      label: 'Twitter',
+                      href: 'https://twitter.com/docusaurus',
+                    },
+                  ],
+                },
+                {
+                  title: 'More',
+                  items: [{
+                      label: 'Blog',
+                      to: '/blog',
+                    },
+                    {
+                      label: 'GitHub',
+                      href: 'https://github.com/facebook/docusaurus',
+                    },
+                  ],
+                },
+              ],
+              copyright: `Copyright © ${new Date().getFullYear()} Loop Language`,
+            },
+            prism: {
+              theme: lightCodeTheme,
+              darkTheme: darkCodeTheme,
+            },
+          }),
+      });
