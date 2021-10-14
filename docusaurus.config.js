68bf6ff1fe83fae7ade27138771744d98b5cee89
Kanersps <kanepetra@gmail.com>
2021-10-14 22:22:05 +0200

    tweak: add trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,126 +1,126 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'Loop Language',
-  tagline: 'Modern dynamic type-safe, programming language.',
-  url: 'https://looplang.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'looplanguage', // Usually your GitHub org/user name.
-  projectName: 'looplanguage.github.io', // Usually your repo name.
+(
+  module.exports = {
+    title: "Loop Language",
+    tagline: "A modern dynamic type-safe, programming language.",
+    url: "https://looplang.org/",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "looplanguage", // Usually your GitHub org/user name.
+    projectName: "looplanguage.github.io", // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            editUrl: "https://github.com/looplanguage/looplanguage.github.io/edit/main",
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl: "https://github.com/looplanguage/looplanguage.github.io/edit/main/blog",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit/main',
+        algolia: {
+          apiKey: "YOUR_API_KEY",
+          indexName: "YOUR_INDEX_NAME",
+
+          // Optional: see doc section below
+          contextualSearch: true,
+
+          // Optional: see doc section below
+          appId: "YOUR_APP_ID",
+
+          // Optional: Algolia search parameters
+          searchParameters: {},
+
+          //... other Algolia params
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/looplanguage/looplanguage.github.io/edit/main/blog',
+        navbar: {
+          title: "Loop",
+          logo: {
+            alt: "Loop Logo",
+            src: "img/logo.png",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "Docs",
+            },
+            {
+              to: "/blog",
+              label: "Updates",
+              position: "left",
+            },
+            {
+              href: "https://github.com/looplanguage",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Introduction",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Twitter",
+                  href: "https://twitter.com/kanersps",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Updates",
+                  to: "/blog",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/looplanguage",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Loop Language`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      algolia: {
-        apiKey: 'YOUR_API_KEY',
-        indexName: 'YOUR_INDEX_NAME',
-
-        // Optional: see doc section below
-        contextualSearch: true,
-
-        // Optional: see doc section below
-        appId: 'YOUR_APP_ID',
-
-        // Optional: Algolia search parameters
-        searchParameters: {},
-
-        //... other Algolia params
-      },
-      navbar: {
-        title: 'Loop',
-        logo: {
-          alt: 'Loop Logo',
-          src: 'img/logo.png',
-        },
-        items: [{
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
-          },
-          {
-            to: '/blog',
-            label: 'Updates',
-            position: 'left'
-          },
-          {
-            href: 'https://github.com/looplanguage',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [{
-            title: 'Docs',
-            items: [{
-              label: 'Introduction',
-              to: '/docs/intro',
-            }, ],
-          },
-          {
-            title: 'Community',
-            items: [{
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [{
-                label: 'Updates',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/looplanguage',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Loop Language`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+  }
+);
