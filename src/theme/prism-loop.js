4cfdadb96798703c02f9af528e6bf6eaedb7c10d
Kanersps <12296463+kanersps@users.noreply.github.com>
2021-11-02 14:30:35 +0100

    feat: update syntax highlighting

--- src/theme/prism-loop.js
+++ src/theme/prism-loop.js
@@ -1,4 +1,39 @@
 (function (Prism) {
+  function keywordsToPattern(words) {
+		return '\\b(?:' + words.trim().replace(/ /g, '|') + ')\\b';
+	}
+
+  function nested(pattern, depthLog2) {
+		for (var i = 0; i < depthLog2; i++) {
+			pattern = pattern.replace(/<<self>>/g, function () { return '(?:' + pattern + ')'; });
+		}
+		return pattern.replace(/<<self>>/g, '[^\\s\\S]');
+	}
+
+  function re(pattern, replacements, flags) {
+		return RegExp(replace(pattern, replacements), flags || '');
+	}
+
+  function replace(pattern, replacements) {
+		return pattern.replace(/<<(\d+)>>/g, function (m, index) {
+			return '(?:' + replacements[+index] + ')';
+		});
+	}
+
+  var keywordKinds = {
+		typeDeclaration: 'struct enum',
+	};
+
+	var typeDeclarationKeywords = keywordsToPattern(keywordKinds.typeDeclaration);
+
+	var keywords = RegExp(keywordsToPattern(keywordKinds.typeDeclaration));
+
+  // types
+  var generic = nested(/<(?:[^<>;=+\-*/%&|^]|<<self>>)*>/.source, 2); // the idea behind the other forbidden characters is to prevent false positives. Same for tupleElement.
+	var nestedRound = nested(/\((?:[^()]|<<self>>)*\)/.source, 2);
+	var name = /@?\b[A-Za-z_]\w*\b/.source;
+	var genericName = replace(/<<0>>(?:\s*<<1>>)?/.source, [name, generic]);
+
   Prism.languages.loop = {
     "comment": [
       {
@@ -16,12 +51,43 @@
       pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
       greedy: true,
     },
-    "keyword": /\b(?:var|return|if|else|import|as|export)\b/,
+    "attributes": [
+      {
+        pattern: /@\w*/,
+        inside: {
+          "class-name": {
+            pattern: /^@\w*/
+          }
+        }
+      }
+    ],
+    "builtin": /\b(print|println)\b/,
     "function": /\b(?:fn)\b/,
     "boolean": /\b(?:false|true)\b/,
     "function": /\b\w+(?=\()/,
     "number": /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
     "operator": /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+    "constant": /\b[A-Z](?:[A-Z_]|\dx?)*\b/,
+    "class-name": {
+      pattern: re(/(\b<<0>>\s+)<<1>>/.source, [typeDeclarationKeywords, genericName]),
+			lookbehind: true,
+    },
+    "keyword": /\b(?:var|return|if|else|import|as|export|enum|struct)\b/,
+    "constant": [
+      {
+        pattern: /\b(?:String|Integer|Float|Boolean)\b/,
+      },
+    ],
+    "index": [
+      {
+        pattern: /\b\w*\.\w*\b/,
+        inside: {
+          'class-name': {
+            pattern: /^[A-Z]\w*/
+          }
+        }
+      }
+    ],
     "punctuation": /[{}[\];(),.:]/,
   };
 
