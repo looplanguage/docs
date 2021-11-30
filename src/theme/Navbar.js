ef04c3d76201b80141c6da9d453829cac002dff9
Kanersps <12296463+kanersps@users.noreply.github.com>
2021-11-30 11:58:06 +0100

    tweak: we talked about it

--- src/theme/Navbar.js
+++ src/theme/Navbar.js
@@ -1,80 +1,100 @@
-import React from "react"
-import useThemeContext from "@theme/hooks/useThemeContext"
-import SearchBar from "@theme/SearchBar"
+import React from "react";
+import useThemeContext from "@theme/hooks/useThemeContext";
+import SearchBar from "@theme/SearchBar";
+import styles from "./navbar.module.css";
 
-function Navbar(){
-    const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
+// Loop logos
+import lightModeLogo from "../../static/img/logoLightMode.svg";
+import darkModeLogo from "../../static/img/logoDarkMode.svg";
 
-    function toggleTheme() {
-        var darkModeCSS = document.getElementById("lightMode");
-        console.log(isDarkTheme)
-        if ( isDarkTheme ) {
-            darkModeCSS.classList.remove("darkMode");  
-            setLightTheme();
-            document.getElementsByClassName('loopLogo')[0].src="../../static/img/logoLightMode.svg"; 
-            document.getElementById('mobLogo').src="../../static/img/logoLightMode.svg";
-            document.getElementsByClassName('githubLogo')[0].src="../../static/img/Github.png"; 
-        } else {
-            setDarkTheme();
-            document.getElementsByClassName('loopLogo')[0].src="../../static/img/logoDarkMode.svg"; 
-            document.getElementById('mobLogo').src="../../static/img/logoDarkMode.svg";
-            document.getElementsByClassName('githubLogo')[0].src="../../static/img/GithubWhite.png"; 
-            darkModeCSS.classList.add("darkMode");
-        }
+// Github logos
+import lightGithub from "../../static/img/GitHubWhite.png";
+import darkGithub from "../../static/img/GitHub.png";
+
+function Navbar() {
+  const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
+
+  function toggleTheme() {
+    //var darkModeCSS = document.getElementById("lightMode");
+    console.log(isDarkTheme);
+    if (isDarkTheme) {
+      // darkModeCSS.classList.remove("darkMode");
+      setLightTheme();
+      // document.getElementsByClassName("loopLogo")[0].src = "../../static/img/logoLightMode.svg";
+      // document.getElementById("mobLogo").src = "../../static/img/logoLightMode.svg";
+      // document.getElementsByClassName("githubLogo")[0].src = "../../static/img/Github.png";
+    } else {
+      setDarkTheme();
+      // document.getElementsByClassName("loopLogo")[0].src = "../../static/img/logoDarkMode.svg";
+      // document.getElementById("mobLogo").src = "../../static/img/logoDarkMode.svg";
+      // document.getElementsByClassName("githubLogo")[0].src = "../../static/img/GithubWhite.png";
+      // darkModeCSS.classList.add("darkMode");
     }
+  }
 
-// dark toevoegen aan classname = click on toggletheme moet -dark
+  // dark toevoegen aan classname = click on toggletheme moet -dark
 
-
-return <nav id="lightMode">
-    <div id="topNav" class="navbar__items navbar__items--right navItemsRight">
-        <p id="textDayNight">Appearance</p>                
-    <div class="LNSwitch r" id="LNSwitch-1">
-          <input onClick={ toggleTheme } type="checkbox" class="checkbox" />
+  return (
+    <nav className={`${styles.lightModeNavBar} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+      <div id="topNav" class="navbar__items navbar__items--right navItemsRight">
+        <p id="textDayNight">Appearance</p>
+        <div class="LNSwitch r" id="LNSwitch-1">
+          <input onClick={toggleTheme} type="checkbox" class="checkbox" />
           <div class="knobs"></div>
           <div class="layer"></div>
-    </div>
-
-    </div>
-    {/* Left Navbar */}
-    <ul id="navItemsLeft" class="navbar__items--left resMob">
-    <img class="loopLogo logoSize" src="../../static/img/logoLightMode.svg"/>
-        <a class="navItem" href="https://looplang.org/docs/intro"><li>Docs</li></a>
-        <a class="navItem" href="https://looplang.org/blog"><li>Updates</li></a>
+        </div>
+      </div>
+      {/* Left Navbar */}
+      <ul className={`${styles.leftNavBar} ${styles.leftNavBar} ${styles.leftNavBar}`} class="navItemsLeft navbar__items--left resMob">
+        <img class="loopLogo logoSize" src={isDarkTheme ? darkModeLogo : lightModeLogo} />
+        <a class="navItem" href="/docs/intro">
+          <li className={styles.leftNavBar}>Docs</li>
+        </a>
+        <a class="navItem" href="/blog">
+          <li>Updates</li>
+        </a>
         {/* <a class="navItem" href="#"><li>About</li></a> */}
-    </ul>
+      </ul>
 
-    {/* Right Navbar */}
-    <div class="navbar__items navbar__items--right navItemsRight resMob">
+      {/* Right Navbar */}
+      <div class="navbar__items navbar__items--right navItemsRight resMob">
         {/* search bar docusaurus */}
-        <SearchBar id="searchBar"/>
-        <a class="navItem" href="https://github.com/looplanguage"><img src="../../static/img/Github.png" class="githubLogo logoSize"/></a>   
-    </div>
+        <SearchBar id="searchBar" />
+        <a class="navItem" href="https://github.com/looplanguage">
+          <img src={isDarkTheme ? lightGithub : darkGithub} class="githubLogo logoSize" />
+        </a>
+      </div>
 
-    {/* Test Mobile Resp Menu */}
+      {/* Test Mobile Resp Menu */}
 
-    <nav role="navigation" id="resHide">
-
-            <div id="menuToggle">
-              <input type="checkbox" />
-                <span></span>
-                <span></span>
-                <span></span>
-            <ul id="menu">
-            <a class="navItem" href="https://looplang.org/docs/intro"><li>Docs</li></a>
-            <a class="navItem" href="https://looplang.org/blog"><li>Updates</li></a>
+      <nav role="navigation" id="resHide">
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <a class="navItem" href="https://looplang.org/docs/intro">
+              <li>Docs</li>
+            </a>
+            <a class="navItem" href="https://looplang.org/blog">
+              <li>Updates</li>
+            </a>
             {/* <a class="navItem" href="#"><li>About</li></a> */}
-        <a class="navItem" href="https://github.com/looplanguage"><li><img src="../../static/img/Github.png" class="githubLogo logoSize"/></li></a>  
-            </ul>
-           </div>
-           <div id="mobMenu">
-    <img id="mobLogo" class="navItem loopLogo logoSize" src="../../static/img/logoLightMode.svg"/>
-    <SearchBar id="searchBar"/>
-    </div>
+            <a class="navItem" href="https://github.com/looplanguage">
+              <li>
+                <img src="../../static/img/Github.png" class="githubLogo logoSize" />
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div id="mobMenu">
+          <img id="mobLogo" class="navItem loopLogo logoSize" src="../../static/img/logoLightMode.svg" />
+          <SearchBar id="searchBar" />
+        </div>
+      </nav>
     </nav>
-    
-
-</nav>
+  );
 }
 
-export default Navbar; 
+export default Navbar;
