a76436f87abad74d39928e1276d2ddf071bc2dd6
Manon <74968409+manontheresa@users.noreply.github.com>
2021-12-06 12:19:28 +0100

    Another test for the images

--- src/theme/Navbar.js
+++ src/theme/Navbar.js
@@ -4,13 +4,13 @@ import SearchBar from "@theme/SearchBar";
 import styles from "../css/navbar.module.css";
 
 // Loop logos
-import lightLoopLogo from "../components/img/loopLogoBlack.png";
-import darkLoopLogo from "../components/img/loopLogoWhite.png";
+import lightLoopLogo from "img/loopLogoBlack.png";
+import darkLoopLogo from "img/loopLogoWhite.png";
 
 
 // Github logos
-import lightGithub from "../components/img/GitHubWhite.png";
-import darkGithub from "../components/img/GitHub.png";
+import lightGithub from "img/GitHubWhite.png";
+import darkGithub from "img/GitHub.png";
 
 // mousedown event 
 // input value checked true or false 
