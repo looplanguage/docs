16932d5ff0e1c9bf678d9bc6c97a8d003036a0d0
WouterPennings <wouterpennings@gmail.com>
2022-01-20 21:37:10 +0100

    fix: image import

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 import "../theme/dracula-prism.css";
-import codeShotsDM from "../../static/img/codeShots.png"
+import codeShotsDM from "../../static/img/codeShotsDM.png"
 import codeShotsNM from "../../static/img/codeShotsNM.png"
 // import HomepagePartners from "../components/HomepagePartners"; May be included in the future, but is inrelevent at the moment
 
